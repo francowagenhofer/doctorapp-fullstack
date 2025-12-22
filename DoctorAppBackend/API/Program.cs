188e9f6fb829bbbcb645a15a61ea4d2781712bba
Franco Wagenhöfer <franwagen.500@gmail.com>
2025-12-21 22:58:10 -0300

    Configuraciónes de modulos y componentes + soporte Cors en API

--- DoctorAppBackend/API/Program.cs
+++ DoctorAppBackend/API/Program.cs
@@ -9,11 +9,17 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Configurar Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Configurar el DbContext con SQL Server
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(o =>
     o.UseSqlServer(connectionString));
+
+// Configurar CORS para permitir solicitudes desde cualquier origen (Angular en este caso)
+builder.Services.AddCors();
 
 
 var app = builder.Build();
@@ -22,7 +28,18 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+// Sirve para que el API pueda ser consumido desde el front-end en Angular
+app.UseCors(x =>
+{
+    x.AllowAnyOrigin()
+           .AllowAnyHeader()
+           .AllowAnyMethod();
+});
 
 app.UseAuthorization();
 
--- DoctorAppFrontend/src/app/app.ts
+++ DoctorAppFrontend/src/app/app.ts
@@ -1,11 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'] // plural
 })
-export class App {
-  protected readonly title = signal('DoctorAppFrontend');
+export class App implements OnInit {
+  title: string = 'DoctorApp';
+  usuarios: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>('http://localhost:5069/api/usuario').subscribe({
+      next: (response: any) => (this.usuarios = response),
+      error: (err: any) => console.error('Error al obtener los usuarios', err),
+      complete: () => console.log('Solicitud completada')
+    });
+  }
 }
